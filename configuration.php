34bfb18ab95678900518411b1c2db5cd23da6715
Pratik <pratikdharam0053@gmail.com>
2024-08-23 02:03:54 +0530

    changed configuration

--- configuration.php
+++ configuration.php
@@ -1,7 +1,7 @@
 <?php
 class JConfig {
 	public $offline = false;
-	public $offline_message = 'This site is down for maintenance.<br>Please check back again soon.';
+	public $offline_message = 'This site is down for maintenance.<br />Please check back again soon.';
 	public $display_offline_message = 1;
 	public $offline_image = '';
 	public $sitename = 'TrimTime';
@@ -9,7 +9,7 @@ class JConfig {
 	public $captcha = '0';
 	public $list_limit = 20;
 	public $access = 1;
-	public $debug = false;
+	public $debug = true;
 	public $debug_lang = false;
 	public $debug_lang_const = true;
 	public $dbtype = 'mysqli';
@@ -28,7 +28,7 @@ class JConfig {
 	public $live_site = '';
 	public $secret = 'EAupjuHUI8qQ9AeA';
 	public $gzip = false;
-	public $error_reporting = 'default';
+	public $error_reporting = 'maximum';
 	public $helpurl = 'https://help.joomla.org/proxy?keyref=Help{major}{minor}:{keyref}&lang={langcode}';
 	public $offset = 'UTC';
 	public $mailonline = true;
@@ -62,4 +62,46 @@ class JConfig {
 	public $session_handler = 'database';
 	public $shared_session = false;
 	public $session_metadata = true;
+	public $memcached_persist = true;
+	public $memcached_compress = false;
+	public $memcached_server_host = 'localhost';
+	public $memcached_server_port = 11211;
+	public $redis_persist = true;
+	public $redis_server_host = 'localhost';
+	public $redis_server_port = 6379;
+	public $redis_server_db = 0;
+	public $cors = false;
+	public $cors_allow_origin = '*';
+	public $cors_allow_headers = 'Content-Type,X-Joomla-Token';
+	public $cors_allow_methods = '';
+	public $behind_loadbalancer = false;
+	public $proxy_enable = false;
+	public $proxy_host = '';
+	public $proxy_port = '';
+	public $proxy_user = '';
+	public $massmailoff = false;
+	public $replyto = '';
+	public $replytoname = '';
+	public $MetaRights = '';
+	public $sitename_pagetitles = 0;
+	public $session_filesystem_path = '';
+	public $session_memcached_server_host = 'localhost';
+	public $session_memcached_server_port = 11211;
+	public $session_redis_persist = 1;
+	public $session_redis_server_host = 'localhost';
+	public $session_redis_server_port = 6379;
+	public $session_redis_server_db = 0;
+	public $session_metadata_for_guest = true;
+	public $frontediting = 1;
+	public $log_everything = 0;
+	public $log_deprecated = 0;
+	public $log_priorities = array('0' => 'all');
+	public $log_categories = '';
+	public $log_category_mode = 0;
+	public $cookie_domain = '';
+	public $cookie_path = '';
+	public $asset_id = '1';
+	public $devmode = 0;
+	public $redis_server_auth = '';
+	public $session_redis_server_auth = '';
 }
